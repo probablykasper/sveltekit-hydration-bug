a5777386a0fd4e805706d6cdc0eef9246f19f121
Kasper <kasperkh.kh@gmail.com>
2022-04-04 19:34:55 +0200

    Remove deps

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,12 +10,3 @@
   <script async src="https://w.appzi.io/w.js?token=LWNsU"></script>
   <!-- End Appzi -->
 {/if}
-
-<style lang="sass">
-  :global(html)
-    height: 100%
-  :global(body)
-    color: #21242c
-    height: 100%
-    background-color: rgb(241, 245, 249)
-</style>
